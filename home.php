abe68644d1f31927b86dca5a322f52c95fe33081
illia-bashchuk <ilvasilich@gmail.com>
2020-12-23 17:49:24 +0200

    add jquery

--- home.php
+++ home.php
@@ -46,7 +46,7 @@ get_header();
         </div>
     </div>
 </section>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+
 <script type="text/javascript">
     jQuery(document).ready(function() {
         jQuery("#gallery").unitegallery(
